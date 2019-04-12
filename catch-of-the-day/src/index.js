b5001ce29be91497c074e3479b7d516a411760cd
jgarciallamas <jgarciallamas@yahoo.es>
2019-04-12 15:54:58 +0200

    StorePicker component created

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,8 @@
-// let's go!
+// import React from 'react';
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from '../src/components/StorePicker';
+
+
+
+render(<StorePicker />, document.querySelector('#main'));
